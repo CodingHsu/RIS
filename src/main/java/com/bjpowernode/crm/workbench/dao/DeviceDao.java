c5a714f6ad6712e9ca21609b1d03eb16376fada0
xhy <xhy@1455096897.com>
2021-07-10 19:31:02 +0800

    登记模块

--- src/main/java/com/bjpowernode/crm/workbench/dao/DeviceDao.java
+++
@@ -1,7 +0,0 @@
-package com.bjpowernode.crm.workbench.dao;
-
-import com.bjpowernode.crm.workbench.domain.Device;
-
-public interface DeviceDao {
-    Device test(String s);
-}
--- src/main/java/com/bjpowernode/crm/workbench/service/Impl/ActivityServiceImpl.java
+++
@@ -1,9 +0,0 @@
-package com.bjpowernode.crm.workbench.service.Impl;
-
-import com.bjpowernode.crm.utils.SqlSessionUtil;
-import com.bjpowernode.crm.workbench.dao.ActivityDao;
-import com.bjpowernode.crm.workbench.service.ActivityService;
-
-public class ActivityServiceImpl implements ActivityService {
-    private ActivityDao activityDao = SqlSessionUtil.getSqlSession().getMapper(ActivityDao.class);
-}
